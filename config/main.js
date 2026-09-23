66060d7e072b41ce452e19d70ddf06e8472b11bd
gregghudson924 <gregghudson924@gmail.com>
2026-09-23 17:14:14 -0500

    Update site name and subtext in main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "SchoolGamz."; // Change this to change the name of your website.
+var subtext = "v1.67"; // set the subtext
 
 // more settings in main.css
 
